e606361127550aaa7644c4e9bca7b75c9eb31152
許郁棋 <roy@Roys-MacBook-12.local>
2016-06-29 14:35:50 +0800

    remove import uikit

--- TWFoundation/TWFoundation/TWFoundation.h
+++ TWFoundation/TWFoundation/TWFoundation.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 Tiny World. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for TWFoundation.
 FOUNDATION_EXPORT double TWFoundationVersionNumber;
